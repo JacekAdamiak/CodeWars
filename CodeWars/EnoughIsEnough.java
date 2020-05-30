0862670b127997e0408aa039910b376cf77ae648
Jacek Adamiak <jacek.adamiak@gazeta.pl>
2020-05-30 22:55:00 +0200

    third kata add

--- CodeWars/EnoughIsEnough.java
+++ CodeWars/EnoughIsEnough.java
@@ -3,7 +3,7 @@ package CodeWars;
 public class EnoughIsEnough {
 
     public static void main(String[] args) {
-        int[] testList = new int[] { 20, 37, 20, 21, 100, 100 };
+        int[] testList = new int[] { 20, 37, 20, 21, 100, 100, 7 };
         int[] newList = deleteNth(testList, 1);
 
         System.out.print("{ ");
@@ -18,7 +18,15 @@ public class EnoughIsEnough {
     }
 
         public static int[] deleteNth(int[] elements, int maxOccurrences) {
-        int[] newElements = elements;
+        int[] newElements = new int[20];
+        int[] tempList = new int[20];
+
+        for (int i=0; i<elements.length; i++) {
+            
+            tempList[i] = elements[i];
+        }
+
+        newElements = tempList;
         return newElements;
     }
 }
